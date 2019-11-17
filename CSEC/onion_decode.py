2a3b4245c469258beec5efe4ac47e4039b3238ab
Brayden <braydenjw00@gmail.com>
2019-11-17 12:06:10 -0500

    Update onion_decode.py

--- CSEC/onion_decode.py
+++ CSEC/onion_decode.py
@@ -9,53 +9,53 @@ def make_string(file):
     return strings
 
 
-def find_base(bruh):
-    temp = bruh[0:]
+def find_base(cipher):
+    temp = cipher[0:]
     base32NoNo = ['0', '1', '8', '9']
-    for char in bruh:
+    for char in cipher:
         if char.islower():
             return '64'
-    for char in bruh:
+    for char in cipher:
         if char in base32NoNo:
             return '16'
     return '32'
 
 def parse(file):
     out = open("output.txt", 'w')
-    bruh = make_string(file)
+    cipher = make_string(file)
     temp = ''
     for i in range(0, 50):
-        base = find_base(bruh)
+        base = find_base(cipher)
         if base == '64':
             print("64")
-            while len(bruh) % 4 != 0:
-                bruh += '='
-            print(len(bruh))
-            bruh = base64.standard_b64decode(bruh)
-            bruh = str(bruh)
-            bruh = bruh[1:]
-            bruh = bruh.strip("\'")
-            out.write(bruh)
+            while len(cipher) % 4 != 0:
+                cipher += '='
+            print(len(cipher))
+            cipher = base64.standard_b64decode(cipher)
+            cipher = str(cipher)
+            cipher = cipher[1:]
+            cipher = cipher.strip("\'")
+            out.write(cipher)
             out.write('\n')
 
         elif base == '32':
             print("32")
-            while len(bruh) % 8 != 0:
-                bruh += '='
-            print(len(bruh))
-            bruh = base64.b32decode(bruh)
-            bruh = str(bruh)
-            bruh = bruh[1:]
-            bruh = bruh.strip("\'")
-            out.write(bruh)
+            while len(cipher) % 8 != 0:
+                cipher += '='
+            print(len(cipher))
+            cipher = base64.b32decode(cipher)
+            cipher = str(cipher)
+            cipher = cipher[1:]
+            cipher = cipher.strip("\'")
+            out.write(cipher)
             out.write('\n')
         else:
             print("16")
-            bruh = base64.b16decode(bruh)
-            bruh = str(bruh)
-            bruh = bruh[1:]
-            bruh = bruh.strip("\'")
-            out.write(bruh)
+            cipher = base64.b16decode(cipher)
+            cipher = str(cipher)
+            cipher = cipher[1:]
+            cipher = cipher.strip("\'")
+            out.write(cipher)
             out.write('\n')
 
     file.close()
