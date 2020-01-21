decc3c6f5aac3505535490ea7f510ea946804e74
PapaChain <braydenjw00@gmail.com>
2020-01-21 11:00:22 -0500

    CSEC Tool?

---
+++ CSEC/FinnaCurveYall/make_key.py
@@ -0,0 +1,7 @@
+# Code Based off of
+# _________________
+# https://github.com/kudelskisecurity/chainoffools
+# https://research.kudelskisecurity.com/2020/01/15/cve-2020-0601-the-chainoffools-attack-explained-with-poc/
+#
+
+from Crypto.IO import PEM
