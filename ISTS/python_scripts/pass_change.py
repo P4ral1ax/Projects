98ae8f3d739fc3d59d0e138e4b4b022f7da2885f
PapaChain <braydenjw00@gmail.com>
2020-01-22 17:59:24 -0500

    Users File

--- ISTS/python_scripts/pass_change.py
+++ ISTS/python_scripts/pass_change.py
@@ -1,8 +1,16 @@
 import os
 
+def make_set(file):
+    name_set = {}
+    for line in file:
+        line = line.strip('\n')
+        name_set += line
+    return(name_set)
 
-oldpass = input("enter the password to the accounts")
-newpass = input("enter the new password to change to")
-
-os.system(awk -F: '$3 >= 1000 {print $1}' /etc/passwd > users.txt)
-file = open(users.txt)
+def main():
+    # oldpass = input("enter the password to the accounts")
+    # newpass = input("enter the new password to change to")
+    # os.system(awk -F: '$3 >= 1000 {print $1}' /etc/passwd > users.txt)
+    file = open(users.txt)
+    names = make_set(file)
+    print(names)
