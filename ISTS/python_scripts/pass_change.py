1f1a53c00a70965913ff6a395420c47765e5df90
PapaChain <braydenjw00@gmail.com>
2020-01-22 18:09:40 -0500

    Working Users

--- ISTS/python_scripts/pass_change.py
+++ ISTS/python_scripts/pass_change.py
@@ -1,16 +1,25 @@
 import os
 
 def make_set(file):
-    name_set = {}
+    name_arr = []
     for line in file:
-        line = line.strip('\n')
-        name_set += line
-    return(name_set)
+        name = line.strip('\n')
+        name_arr.append(name)
+    return(name_arr)
+
+
+def pass_change(list):
+    for name in list:
+        pass
+
 
 def main():
     # oldpass = input("enter the password to the accounts")
     # newpass = input("enter the new password to change to")
     # os.system(awk -F: '$3 >= 1000 {print $1}' /etc/passwd > users.txt)
-    file = open(users.txt)
+    file = open("users.txt")
     names = make_set(file)
-    print(names)
+
+
+
+main()
