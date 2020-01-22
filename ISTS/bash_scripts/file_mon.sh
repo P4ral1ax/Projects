c957468edc9a215d53f9da3a16a016b150bb0a99
PapaChain <braydenjw00@gmail.com>
2020-01-21 22:46:10 -0500

    Remove Broke Code

--- ISTS/bash_scripts/file_mon.sh
+++ ISTS/bash_scripts/file_mon.sh
@@ -1,7 +1,3 @@
 #! /bin/bash
 # This program will monitor a single file or directory
 # Hopefully will offer options of any, time, size, or SID changes
-## get UID limit ##
-l=$(grep "^UID_MIN" /etc/login.defs)
-## use awk to print if UID >= $UID_LIMIT ##
-awk -F':' -v "limit=${l##UID_MIN}" '{ if ( $3 >= limit ) print $1}' /etc/passwd
