2d3fed62b8a2ae0acc35634fc413b2702d809816
PapaChain <braydenjw00@gmail.com>
2020-01-21 22:45:16 -0500

    Password

--- ISTS/bash_scripts/pass_change.sh
+++ ISTS/bash_scripts/pass_change.sh
@@ -2,3 +2,24 @@
 # This program will automatically change the passwords to all the
 # users on a linux system instantaneously
 # Will list each account the password is changed to
+
+# Get the password to change to
+echo "enter the password to the accounts"
+read oldpass
+echo "Please enter the password to change to"
+read newpass
+
+#make file of each username
+$(awk -F: '$3 >= 1000 {print $1}' /etc/passwd > users.txt)
+filecontent=( 'cat "users.txt"')
+
+#load it into an array
+readarray userarray < "users.txt"
+
+# change each password
+for u in "${userarray[5]}" ; do
+  u=${u%$'\n'}
+  echo "$u:$newpass | sudo chpasswd"
+
+  # echo "Password Changed for : $u"
+done
