489a882b58ebc13c836abdec34503a1b4eff4718
PapaChain <braydenjw00@gmail.com>
2020-01-23 08:01:38 -0500

    AIM

---
+++ ISTS/python_scripts/file_mon.py
@@ -0,0 +1,16 @@
+"""
+This program will monitor directories and files to see if they were edited
+or changed in any way. If there are any changes it alerts the user and
+logs the action.
+
+Hopeful Features:
+ - Normal file updates
+    - File size, date made, deletion
+    - Permissions Changes
+    - If files opened
+ - Directory monitoring
+    - Monitors each file In Directory
+    - Monitors any file deletions or creation
+    - Monitors Directory Permissions
+    - Directories inside of the root Directory
+"""
