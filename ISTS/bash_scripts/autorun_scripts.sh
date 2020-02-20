e409743dd2232e5d658fdfbfb0233361bf0090b2
PapaChain <braydenjw00@gmail.com>
2020-02-20 11:07:04 -0500

    Update Scripts

--- ISTS/bash_scripts/autorun_scripts.sh
+++ ISTS/bash_scripts/autorun_scripts.sh
@@ -7,18 +7,22 @@ echo 'Password Change Quickrun.....'
 $(sudo python3 ../pass_change/pass_change.py)
 echo '1'
 
-echo 'file_mon with default.....'
-$(sudo python3 ../file-mon/file-mon.py)
-echo '1'
+echo 'Set Firewall....'
+#$(sudo python3 firewall_default.py)
+
+echo 'SSH Secure....'
+#$(sudo python3 ssh_secure.py)
 
 echo 'Secure Settings....'
 #$(sudo python3 default_settings.py)
 
-echo 'SSH Secure....'
-#$(sudo python3 .ssh_secure.py)
+echo 'file_mon with default.....'
+$(sudo python3 ../file-mon/file-mon.py)
+echo '1'
 
 echo 'Raw Detect.....'
 #$(sudo python3 raw_detect.py)
 
-echo 'Set Firewall....'
-#$(sudo python3 firewall_default.py)
+echo 'Get Security Software......'
+#$(apt-get ...)
+#$(apt-get install ...)
---
+++ ISTS/python_scripts/set-firewall/set-firewall.py
@@ -0,0 +1,19 @@
+""" Script to set basic firewall rules for the start of the competition
+
+
+Services Needed:
+
+    ICMP
+    HTTP - 80
+    HTTPS - 443
+    SSH - 22
+    VNC - 7900-7902
+    IMAP - 143, 993
+    SMTP - 25, 2525, 465
+    XMPP
+    ES?
+    TOR Entry Node?
+
+
+Detect computer from IP address? or manually type.
+"""
