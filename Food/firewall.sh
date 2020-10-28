365c04069d9a1d821f9f15fe92a48673a914306f
PapaChain <braydenjw00@gmail.com>
2020-10-28 08:51:17 -0400

    Added Example=, Fixed Script

--- Food/firewall.sh
+++ Food/firewall.sh
@@ -14,12 +14,26 @@ start() {
     iptables -A OUTPUT -j DROP
 
     # Ping is Good 
-    iptables -A INPUT -p icmp -j ACCEPT
-    iptables -a OUTPUT -p icmp -j ACCEPT
+    iptables -A INPUT -p ICMP -j ACCEPT
+    iptables -A INPUT -p ICMP -j ACCEPT
+
+    # Allow Loopback
+    iptables -A INPUT -i lo -j ACCEPT
+    iptables -A OUTPUT -o lo -j ACCEPT
 
     # Accept SSH
-    iptables -A -INPUT -p tcp --dport ssh -m state --state NEW,ESTABLISHED -j ACCEPT
+    iptables -A INPUT -p tcp --dport ssh -m state --state NEW,ESTABLISHED -j ACCEPT
     iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 
-    
+    # Accept Chat Thinggy
+    iptables -A INPUT -p tcp --dport 3000 -m state --state NEW,ESTABLISHED -j ACCEPT
+    iptables -A OUTPUT -p tcp --sport 3000 -m state --state ESTABLISHED -j ACCEPT
+
+    # Accept MongoDB
+    iptables -A INPUT -p tcp --dport 27017 -m state --state NEW,ESTABLISHED -j ACCEPT
+    iptables -A OUTPUT -p tcp --sport 27017 -m state --state ESTABLISHED -j ACCEPT
+
+    sleep(3)
+
+    iptables -F
 }
